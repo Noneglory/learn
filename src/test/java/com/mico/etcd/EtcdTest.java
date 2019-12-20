de16c9c8c2f090d267d712ecef63982a05439145
kexi <Whk4jfp18VoKZ_cN1gdpTY>
2019-12-20 18:45:40 +0800

    etcd简单集成

---
+++ src/test/java/com/mico/etcd/EtcdTest.java
@@ -0,0 +1,33 @@
+package com.mico.etcd;
+
+import com.mico.basic;
+import com.mico.learn.LearnApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.ExecutionException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LearnApplication.class)
+public class EtcdTest {
+    @Autowired
+    private EtcdDao etcdDao;
+
+    @Test
+    public void setKeyValueTest() throws ExecutionException, InterruptedException {
+        etcdDao.setKeyValue("/name/message/top","aaaaaaaaa");
+    }
+
+    @Test
+    public void getEtcdValueByKeyTest(){
+        try {
+            String ss = etcdDao.getEtcdValueByKey("/name/message/top");
+            System.out.println(ss);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
